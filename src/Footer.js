75d1441c7157dc0b3d3f1b5890dca05a638e796c
Het <hp.2k22@icloud.com>
2022-09-04 22:00:27 +0530

    Made an airbnb-clone

---
+++ src/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Footer.css'
+
+function Footer() {
+    return (
+        <div className='footer'>
+            <p>© 2022 Airbnb clone! -20DCS077</p>
+            <p>Privacy · Terms </p>
+        </div>
+    )
+}
+
+export default Footer
